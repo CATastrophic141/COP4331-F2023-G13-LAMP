5fe80df0ab7639c585fcb8d388ae258709f61ec6
Rahul Mohan <143127250+rmohan-ucf@users.noreply.github.com>
2023-08-25 22:50:06 -0400

    Updated MySQL config, bug fixes, and whitespace formatting

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -2,20 +2,21 @@
 	require_once("Config.php");
 
 	$inData = getRequestInfo();
-	
+
 	$conn = new mysqli(DB_HOSTNAME, DB_USER, DB_PASSWORD, DB_NAME);
-	if ($conn->connect_error) 
+	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
-	} 
+	}
 	else
 	{
 		$stmt = $conn->prepare("INSERT into Contacts (UserId,Name,Phone,Email) VALUES(?,?,?,?)");
 		$stmt->bind_param("ssss", $inData["userId"], $inData["contactName"], $inData["contactPhone"], $inData["contactEmail"]);
 		$stmt->execute();
+		$contactId = $stmt->insert_id;
 		$stmt->close();
 		$conn->close();
-		returnWithError("");
+		returnWithInfo($contactId);
 	}
 
 	function getRequestInfo()
@@ -28,11 +29,17 @@
 		header("Content-type: application/json");
 		echo $obj;
 	}
-	
+
 	function returnWithError( $err )
 	{
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
+
+	function returnWithInfo( $id )
+	{
+		$retValue = '{"id":' . $id . ',"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+
 ?>
