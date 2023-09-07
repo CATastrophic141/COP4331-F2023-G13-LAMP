1d6caab120cb11970a4d4d4a86e0efdce41fde3e
calablammo3116 <109045651+calablammo3116@users.noreply.github.com>
2023-09-07 18:30:39 -0400

    Deleted html folder and moved file contents

--- js/code.js
+++ js/code.js
@@ -251,7 +251,7 @@ function doLogout()
 	firstName = "";
 	lastName = "";
 	//document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-	window.location.href = "index.html";
+	window.location.href = "./index.html";
 }
 
 function makeTableRow(table, contactJSON){
